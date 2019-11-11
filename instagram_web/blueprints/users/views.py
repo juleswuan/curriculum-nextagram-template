cff4f489a89a45abb6158ee0804c2fc881c9527f
Juliana Wuan <julianawuan@gmail.com>
2019-11-11 13:59:28 +0800

    complete sign up form validation. correct error message flashing

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -15,25 +15,20 @@ def new():
 
 @users_blueprint.route('/', methods=['POST'])
 def create():
-    # user = User(**request.form.to_dict())
-    # if user.save():
-    #     print(user.username, user.email, user.password)
-
-    password = request.form.get('password')
-    hashed = generate_password_hash(password)
 
     username = request.form.get('name')
-    if username.match("^[a-zA-Z0-9_.-]+$", username) and len(username) >= 8:
-        user = User(username=username, email=request.form.get(
-            'email'), password=hashed)
-        if user.save(): # ok to save to db
-            flash('New user has been added!')
-            return redirect(url_for('users.new'))
-        else:
-            return render_template ('users/new.html')
-    else:
-        flash('Invalid username.\nPlease choose a username that is: \nmin. 8 chars in length\ncontains only alphanumeric chars (a-z, A-Z, 0-9) and (_ . -)')
-        return render_template ('users/new.html')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user = User(username=username, email=email, password=password) 
+
+    if user.save():
+        flash('New user has been added!')
+        return redirect(url_for('users.new'))
+    else: 
+        for error in user.errors:
+            flash(error)
+        return render_template ('users/new.html', username=username, email=email, password=password)
 
 
 @users_blueprint.route('/<username>', methods=["GET"])
--- models/user.py
+++ models/user.py
@@ -1,5 +1,7 @@
 from models.base_model import BaseModel
 import peewee as pw
+import re
+from werkzeug.security import generate_password_hash
 
 
 class User(BaseModel):
@@ -7,3 +9,28 @@ class User(BaseModel):
     email = pw.CharField(null=False, unique=True)
     password = pw.CharField(null=False)
 
+    def validate(self):
+        # check username constraints
+        duplicate_username = User.get_or_none(User.username == self.username)
+
+        if duplicate_username:
+            self.errors.append('Username not unique')
+        elif len(self.username) < 6 or len(self.username) > 20:
+            self.errors.append('Username must be between 6 and 20 chars')
+        elif not re.match(r"^[a-zA-Z0-9_.-]+$", self.username):
+            self.errors.append('Username must contain ONLY alphanumeric chars (a-z, A-Z, 0-9) and underscore / dot / dash symbols (_ . -)')
+
+        # check email constraints
+        duplicate_email = User.get_or_none(User.email == self.email)
+
+        if duplicate_email:
+            self.errors.append('Email is already in use. Please enter another email')
+        elif not re.match(r"[^@]+@[^@]+\.[^@]+", self.email):
+            self.errors.append('Email not in correct format')
+
+        # check password constraints
+        if not re.match(r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$" , self.password):
+            self.errors.append('Password must be min. 8 chars, and contain min. one letter, one number and one special character')
+        else:
+            self.password = generate_password_hash(self.password)
+        
