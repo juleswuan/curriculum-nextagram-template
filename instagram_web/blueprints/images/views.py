c067d49ed007f6e6cc5e96f6b5a3bd0d56b9e993
Juliana Wuan <julianawuan@gmail.com>
2019-11-14 15:39:40 +0800

    build images model. WIP image upload form

--- instagram_web/blueprints/images/views.py
+++ instagram_web/blueprints/images/views.py
@@ -4,3 +4,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 images_blueprint = Blueprint('images',
                             __name__,
                             template_folder='templates')
+
+@images_blueprint.route('/', methods=['GET'])
+def new():
+    return render_template('new.html')
--- models/image.py
+++ models/image.py
@@ -1,4 +1,8 @@
 from models.base_model import BaseModel
+from models.user import User
 import peewee as pw
 
 
+class Image(BaseModel):
+    image_url = pw.CharField()
+    user = pw.ForeignKeyField(User, backref='images')
